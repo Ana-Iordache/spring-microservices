29cdb976ec3f221698297e4a71b91fb3fa1ca377
Iordache Ana-Maria <anaiordache264@gmail.com>
2023-01-12 13:18:15 +0200

    added h2 database

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -1,5 +1,8 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,9 +14,15 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
 public class CurrencyExchange {
+    @Id
     private long id;
+
+    @Column(name = "currency_from")
     private String from;
+
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private String environment; // to know what instance we call
