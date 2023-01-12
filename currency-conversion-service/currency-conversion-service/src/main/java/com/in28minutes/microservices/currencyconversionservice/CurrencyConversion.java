308e0de1325fca9373e44d171982cd396960974f
Iordache Ana-Maria <anaiordache264@gmail.com>
2023-01-12 14:11:32 +0200

    added currency conversion

---
+++ currency-conversion-service/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversion.java
@@ -0,0 +1,22 @@
+package com.in28minutes.microservices.currencyconversionservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CurrencyConversion {
+    private long id;
+    private String from;
+    private String to;
+    private BigDecimal quantity;
+    private BigDecimal conversionMultiple;
+    private BigDecimal totalCalculatedAmount;
+    private String environment;
+}
