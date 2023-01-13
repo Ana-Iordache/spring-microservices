e2e21fe477acf87a6b0ce3c8e3337bdcdd48cc2b
Iordache Ana-Maria <anaiordache264@gmail.com>
2023-01-13 11:07:45 +0200

    feign client (proxy for currency exchange) and connecting services to naming server registry (eureka)

--- currency-conversion-service/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,5 +1,6 @@
 package com.in28minutes.microservices.currencyconversionservice;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,9 +10,12 @@ import org.springframework.web.client.RestTemplate;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+@RequiredArgsConstructor
 @RestController
 public class CurrencyConversionController {
-    @GetMapping("/currency-exchange/from/{from}/to/{to}/quantity/{quantity}")
+    private final CurrencyExchangeProxy proxy;
+
+    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(
             @PathVariable String from,
             @PathVariable String to,
@@ -21,10 +25,11 @@ public class CurrencyConversionController {
         uriVariables.put("from", from);
         uriVariables.put("to", to);
         // this will map all the common fields from CurrencyExchange to CurrencyConversion
-        // and mapp the values of "from" and "to"
-        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
-                CurrencyConversion.class,
-                uriVariables);
+        // and map the values of "from" and "to"
+        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate()
+                .getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+                        CurrencyConversion.class,
+                        uriVariables);
         CurrencyConversion currencyConversion = responseEntity.getBody();
         BigDecimal totalCalculatedAmount = quantity.multiply(currencyConversion.getConversionMultiple());
 
@@ -32,6 +37,22 @@ public class CurrencyConversionController {
                 from, to, quantity,
                 currencyConversion.getConversionMultiple(),
                 totalCalculatedAmount,
-                currencyConversion.getEnvironment());
+                currencyConversion.getEnvironment() + " rest template");
+    }
+
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity
+    ) {
+        CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+        BigDecimal totalCalculatedAmount = quantity.multiply(currencyConversion.getConversionMultiple());
+
+        return new CurrencyConversion(currencyConversion.getId(),
+                from, to, quantity,
+                currencyConversion.getConversionMultiple(),
+                totalCalculatedAmount,
+                currencyConversion.getEnvironment() + " feign");
     }
 }
---
+++ currency-conversion-service/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -0,0 +1,21 @@
+package com.in28minutes.microservices.currencyconversionservice;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "currency-exchange", url = "localhost:8000")
+// the same app name from currency exchange service
+// it's not ok to hardcode the url, because if we have multiple instances of microservices we should modify it everytime
+// or, we can add all de ports oh those services, but in case of one of them is down we also have to modify
+// !all the instances of all microservices will register with a service registry
+// EXAMPLE: if the CurrencyConversion wants to talk to the CurrencyExchange,
+// it will ask the ServiceRegistry(NamingServer) which are the active address of the CurrencyExchange
+// and the ServiceRegistry(NamingServer) will return those address back to the CurrencyConversion
+// and then the CurrencyConversion can send a request to CurrencyExchange
+// LoadBalancer is doing this
+public interface CurrencyExchangeProxy {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    CurrencyConversion retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
+}
