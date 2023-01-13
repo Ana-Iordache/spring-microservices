d01053adce997d60f739277ef4f9344874392009
Iordache Ana-Maria <anaiordache264@gmail.com>
2023-01-13 13:14:28 +0200

    client side load balancing

--- currency-conversion-service/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -4,7 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange", url = "localhost:8000")
+//@FeignClient(name = "currency-exchange", url = "localhost:8000")
+@FeignClient(name = "currency-exchange") // Client side Load Balancing *
 // the same app name from currency exchange service
 // it's not ok to hardcode the url, because if we have multiple instances of microservices we should modify it everytime
 // or, we can add all de ports oh those services, but in case of one of them is down we also have to modify
@@ -19,3 +20,7 @@ public interface CurrencyExchangeProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     CurrencyConversion retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
 }
+// * Client side Load Balancing - automatically done by FeignClient and Eureka
+// inside the Currency Conversion microservice there is a Loader Balancer which is talking to
+// the Naming Server finding the instances and doing automatically load balancing between them
+// (Currency Exchange - instance 1 and Currency Exchange - instance 2)
